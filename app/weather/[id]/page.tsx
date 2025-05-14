46449fb8a8c3ec2e24791feae4c8108d4d1835ed
sathwik pentakoti <sathwikpentakoti@gmail.com>
2025-05-15 02:02:04 +0530

    updated

--- app/weather/[id]/page.tsx
+++ app/weather/[id]/page.tsx
@@ -1,11 +1,5 @@
 import WeatherClient from "./WeatherClient";
 
-type PageProps = {
-  params: {
-    id: string;
-  };
-};
-
-export default function Page({ params }: PageProps) {
+export default function Page({ params }: { params: { id: string } }) {
   return <WeatherClient cityId={params.id} />;
 }
